55bab08ffd8da19d81ce2d86a5230db48d877c34
yongbin1 <rhdydqls1024@naver.com>
2023-04-14 11:19:47 +0900

    :memo: :: 태블릿 종류 추가

--- src/main/java/com/dgsw/equipment/domain/equipment/domain/enums/EquipmentType.java
+++ src/main/java/com/dgsw/equipment/domain/equipment/domain/enums/EquipmentType.java
@@ -10,7 +10,8 @@ public enum EquipmentType {
     PHONE("스마트폰"),
     LAPTOP("노트북"),
     DESKTOP("데스크탑"),
-    MONITOR("모니터");
+    MONITOR("모니터"),
+    TABLET("태블릿");
 
     private final String type;
 
